a78e5287f055d30c7eda5550210c88538d9aa124
Jonathan Mourtada <jonathan.mourtada@gmail.com>
2019-12-03 20:58:00 +0100

    day 1 part 1

--- 1/1.ts
+++ 1/1.ts
@@ -1,3 +1,5 @@
-export function run(input: string): string {
-  return "";
+export function run(input: ReadonlyArray<string>): string {
+  return input
+    .reduce((sum, entry) => sum + (Math.floor(parseInt(entry, 10) / 3) - 2  ), 0)
+    .toString();
 }
